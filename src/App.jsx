27907fa45f85e28d07b66e89ed223359634dfce3
bhggvgg641-source <237578373+bhggvgg641-source@users.noreply.github.com>
2025-11-20 16:22:30 -0500

    Attempt to fix white screen by simplifying App.jsx to load AIRecommendationsPage directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AIRecommendationsPage from './pages/AIRecommendationsPage.jsx';
-import SmartSearchPage from './pages/SmartSearchPage.jsx';
-import VirtualTryOnPage from './pages/VirtualTryOnPage.jsx';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AIRecommendationsPage />} />
-        <Route path="/search" element={<SmartSearchPage />} />
-        <Route path="/try-on" element={<VirtualTryOnPage />} />
-        {/* يمكنك إضافة مسارات أخرى هنا */}
-      </Routes>
-    </Router>
+    <AIRecommendationsPage />
   );
 }
 
